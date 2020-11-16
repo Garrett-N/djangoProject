0920884e28805acd4d2e4937ee553461d88404c8
Garrett N <garrettn@gmail.com>
2020-11-16 17:43:17 -0600

    Setting up template and template inheritance.

--- k9gbnApp/views.py
+++ k9gbnApp/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def Home(request):
-    return HttpResponse("<h1>This is the main page</h1>")
+    #return HttpResponse("<h1>This is the main page</h1>")
+    return render(request, 'home.html')
 
 def K9gbn(request):
     return HttpResponse("<h1>k9gbn app</h1>")
