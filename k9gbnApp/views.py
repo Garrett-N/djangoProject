aad136fbbd9a2a9c75b5b12f7517b8568c337c78
Garrett N <garrettn@gmail.com>
2020-11-18 01:40:27 -0600

    adding some types to my context.

--- k9gbnApp/views.py
+++ k9gbnApp/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def Home(request):
     context = {
-        "name":"Garrett N"
+        "name":"Garrett N",
+        "lucky_number":59,
+        "list_friends":["steve", "gary", "pam", "sue"]
+
     }
     return render(request, 'home.html', context)
 
